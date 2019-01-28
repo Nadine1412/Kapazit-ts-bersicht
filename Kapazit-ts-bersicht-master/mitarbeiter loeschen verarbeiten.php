ecd42738c6ef6ed98e1fbbd8f99e47738a43f535
moehrlef <45455368+moehrlef@users.noreply.github.com>
2019-01-28 13:05:48 +0100

    Update mitarbeiter loeschen verarbeiten.php

--- Kapazit-ts-bersicht-master/mitarbeiter loeschen verarbeiten.php
+++ Kapazit-ts-bersicht-master/mitarbeiter loeschen verarbeiten.php
@@ -10,14 +10,14 @@ define("DB_DATABASE", "kapauebersicht_db");
 
 $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)or die(mysql_error());
 
- //Mitglieder löschen
+ //Mitarbeiter löschen
  $query1 ="DELETE FROM mitarbeiter_tbl WHERE M_ID LIKE '$selectedMid' ";
 
 $result = mysqli_query($db, $query1);
 
 if($result)
 {
-     # Weiterleitung auf die Mitglied löschen Seite;
+     # Weiterleitung auf die Mitarbeiter löschen Seite;
     header('location: mitarbeiter loeschen.php');
     exit(1);
 
